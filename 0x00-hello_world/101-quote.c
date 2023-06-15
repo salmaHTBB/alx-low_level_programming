80bc8572bd855bc18b75b18e16feb81f5b591194
salmaHTBB <rahilisalma93@gmail.com>
2023-06-15 16:00:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints the size of various computer types
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+return (1);
+}
