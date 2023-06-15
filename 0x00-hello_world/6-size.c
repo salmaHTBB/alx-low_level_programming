8b009d140becaab47eb8ce6fd82bc14d9bf0149b
salmaHTBB <rahilisalma93@gmail.com>
2023-06-15 15:23:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  * Return: Always 0 (Success)
  */
 int main(void){
+
 printf("size of a char: %lu byte(s)\n", sizeof(char));
 printf("size of a int: %lu byte(s)\n", sizeof(int));
 printf("size of a long int: %lu byte(s)\n", sizeof(long int));
