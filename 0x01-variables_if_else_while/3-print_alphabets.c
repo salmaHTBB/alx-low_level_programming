ea5b71892ff27acf7b74230c19c4a1da5facebb1
salmaHTBB <rahilisalma93@gmail.com>
2023-06-16 06:29:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
         char letter;
 
         for(letter = 'a' ; letter <= 'z' ; letter++)
-        putchar(letter);
+                putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
