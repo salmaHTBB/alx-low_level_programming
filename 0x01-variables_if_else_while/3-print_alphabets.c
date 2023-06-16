707d3132bf8bb9fbe4ed911789f403f30d1be8f5
salmaHTBB <rahilisalma93@gmail.com>
2023-06-16 06:31:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for(letter = 'a' ; letter <= 'z' ; letter++)
-                putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
